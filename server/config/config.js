1a470dd2223a1544678c07f3ac2a84a213a9365c
German <germassera@gmail.com>
2020-06-24 16:34:55 -0300

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://joseger:Uvlj8pifhChpFN3N@cluster0-dq13c.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
