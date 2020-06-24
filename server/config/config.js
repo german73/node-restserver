631683c6792a7adff3cef3eeb3ba8183d11cd418
German <germassera@gmail.com>
2020-06-24 15:30:22 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 //      Puerto
 // ==============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==============================
+//      Entorno
+// ==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============================
+//      Base de datos
+// ==============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://joseger:Uvlj8pifhChpFN3N@cluster0-dq13c.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
